41672919f67b6c4e981dea59ee146aaaea306e79
TheMOON <mert__balkan@hotmail.com>
2022-02-11 23:17:28 +0300

    Enemy Movement

--- HUJAM1/Assets/Scripts/Concretes/Controllers/EnemyController.cs
+++ HUJAM1/Assets/Scripts/Concretes/Controllers/EnemyController.cs
@@ -5,9 +5,26 @@ namespace HUJAM1.Concretes.Controllers
 {
     public class EnemyController : RotateComponent
     {
+        private float _moveSpeed;
+        private CharacterController2D _character;
+
+        private void Awake()
+        {
+            _character = FindObjectOfType<CharacterController2D>();
+        }
+        private void OnEnable()
+        {
+            _moveSpeed = Random.Range(2.0f, 6.0f);
+        }
         protected override void Update()
         {
             base.Update();
+            MoveToPlayer();
+
+        }
+        private void MoveToPlayer()
+        {
+            this.transform.position = Vector3.Lerp(transform.position, _character.transform.position, (Time.deltaTime / _moveSpeed));
         }
     }
 }
