b3a707a13350726bf766ba01e25af0fd4e473ec7
TheMOON <mert__balkan@hotmail.com>
2022-02-12 13:56:25 +0300

    Border problem fixed

--- HUJAM1/Assets/Scripts/Concretes/Levels/Level1Script.cs
+++ HUJAM1/Assets/Scripts/Concretes/Levels/Level1Script.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using HUJAM1.Concretes.Controllers;
 using UnityEngine;
 
 namespace HUJAM1.Concretes.Levels
 {
     public class Level1Script : MonoBehaviour
     {
+        private CharacterController2D _character;
+        private Vector3 _characterPosition;
+        private void Awake()
+        {
+            _character = FindObjectOfType<CharacterController2D>();
+        }
+        private void Start()
+        {
+        }
         private void Update()
         {
             PassLevel();
+            ForceBorder();
+        }
+        private void ForceBorder()
+        {
+            _characterPosition = _character.transform.position;
+
+            _character.transform.position = new Vector3(
+                Mathf.Clamp(_characterPosition.x, -40.0f, 40.0f),
+                Mathf.Clamp(_characterPosition.y, -40.0f, 40.0f),
+                Mathf.Clamp(_characterPosition.z, -40.0f, 40.0f)
+            );
         }
         private void PassLevel()
         {
